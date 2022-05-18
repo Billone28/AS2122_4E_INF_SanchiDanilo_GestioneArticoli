9c95b27aef485351a113ce58b1cbfd78968d000d
Danilo Sanchi <danilo.sanchi@studenti.ittsrimini.edu.it>
2022-05-18 10:44:27 +0200

    completamento della library

--- AS2122_4E_INF_SanchiDanilo_GestioneArticoli.Library/Class1.cs
+++ AS2122_4E_INF_SanchiDanilo_GestioneArticoli.Library/Class1.cs
@@ -2,6 +2,25 @@
 {
     public class Class1
     {
+        class Articolo
+        {
+            string codice;
+            string descrizione;
+            string UnitadiMisura;
+            string prezzo;
+
+            public Articolo(string codice, string descrizione, string unitadiMisura, string prezzo)
+            {
+                this.codice = codice;
+                this.descrizione = descrizione;
+                this.UnitadiMisura = unitadiMisura;
+                this.prezzo = prezzo;
+            }
+            public string Codice { get { return codice; } }
+            public string Descrizione { get { return descrizione; } }
+            public string UnitaDiMisura { get { return UnitaDiMisura; } }
+            public string Prezzo { get { return prezzo; } }
+        }
 
     }
 }
--- AS2122_4E_INF_SanchiDanilo_GestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_SanchiDanilo_GestioneArticoli/Articolo.cs
@@ -8,21 +8,22 @@ namespace AS2122_4E_INF_SanchiDanilo_GestioneArticoli
 {
     class Articolo
     {
-        string codice;
-        string descrizione;
-        string UnitadiMisura;
-        string prezzo;
+            string codice;
+            string descrizione;
+            string UnitadiMisura;
+            string prezzo;
 
-        public Articolo(string codice, string descrizione, string unitadiMisura, string prezzo)
-        {
-            this.codice = codice;
-            this.descrizione = descrizione;
-            this.UnitadiMisura = unitadiMisura;
-            this.prezzo = prezzo;
+            public Articolo(string codice, string descrizione, string unitadiMisura, string prezzo)
+            {
+                this.codice = codice;
+                this.descrizione = descrizione;
+                this.UnitadiMisura = unitadiMisura;
+                this.prezzo = prezzo;
+            }
+            public string Codice { get { return codice; } }
+            public string Descrizione { get { return descrizione; } }
+            public string UnitaDiMisura { get { return UnitaDiMisura; } }
+            public string Prezzo { get { return prezzo; } }
         }
-        public string Codice { get { return codice; } }
-        public string Descrizione { get { return descrizione; } }
-        public string UnitaDiMisura { get { return UnitaDiMisura; } }
-        public string Prezzo { get { return prezzo; } }
     }
-}
+
